602d31721a439c476aa19fcb284b6ea9c4f5fa52
VasylBarna <vasylbarna@gmail.com>
2021-10-07 16:07:32 +0300

    Основи редукса вебінар група 18

---
+++ src/redux/todos/todos-types.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  ADD: "todos/add",
+  DELETE: "todos/delete",
+  TOGGLE_COMPLETED: "todos/toggleCompleted",
+  CHANGE_FILTER: "todos/changeFilter",
+};
