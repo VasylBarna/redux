b351e55a2bb96b9f01054f7cc096f188144a05e3
VasylBarna <vasylbarna@gmail.com>
2021-10-14 19:29:42 +0300

    webinar grupa18

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import TodosView from "./views/TodosView";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <ul>
+      <li>
+        <Link to="/todos">Заметки</Link>
+      </li>
+    </ul>
+
+    <Switch>
+      <Route path="/todos">
+        <TodosView />
+      </Route>
+    </Switch>
+  </>
+);
 
 export default App;
